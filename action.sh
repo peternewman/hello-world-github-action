d27037b5c243499098c7198d7ecc103da43230d3
Kirill <g4s8.public@gmail.com>
2019-09-13 12:41:13 +0300

    merge & push

--- action.sh
+++ action.sh
@@ -8,6 +8,17 @@ if [[ "${GITHUB_EVENT_NAME}" != "pull_request_review" ]]; then
   exit 1
 fi
 
-USER=$(jq -r .review.user.login ${GITHUB_EVENT_PATH})
-echo "reviewer is ${USER}"
+user=$(jq -r .review.user.login ${GITHUB_EVENT_PATH})
+cmd=$(jq -r .review.body ${GITHUB_EVENT_PATH})
+echo "reviewer is ${user}, command is ${cmd}"
+
+if [[ "${cmd}" == "merge" ]]; then
+  head=$(jq -r .pull_request.head.ref ${GITHUB_EVENT_PATH})
+  git config user.email me@rultor.com
+  git config user.name rultor
+  git checkout -B __rultor origin/${head}
+  git checkout -B master origin/master
+  git merge --no-ff __rultor
+  git push origin master
+fi
 exit 0
